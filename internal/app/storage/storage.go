45c3723bf6cde93270bce8e6d7c6c9524409cc43
Артем <art04.04@mail.ru>
2023-10-22 15:13:54 +0300

    fix

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -10,7 +10,7 @@ func NewStorage(cnf *config.Config) (domains.Storage, error) {
 	if cnf.DatabaseDsn != "" {
 		db, err := NewDB(cnf)
 		if err != nil {
-			return nil, fmt.Errorf("Don`t create db %w", err)
+			return nil, fmt.Errorf("don`t create db %w", err)
 		}
 		return &db, nil
 	}
@@ -18,16 +18,16 @@ func NewStorage(cnf *config.Config) (domains.Storage, error) {
 	if cnf.Memory != "" {
 		writer, err := NewWriter(cnf.Memory)
 		if err != nil {
-			return nil, fmt.Errorf("Don`t create file %w", err)
+			return nil, fmt.Errorf("don`t create file %w", err)
 		}
 
 		reader, err := NewReader(cnf.Memory)
 		if err != nil {
-			return nil, fmt.Errorf("Don`t create file for read %w", err)
+			return nil, fmt.Errorf("don`t create file for read %w", err)
 		}
 		m, err := reader.ReadURL()
 		if err != nil {
-			return nil, fmt.Errorf("Don`t read file %w", err)
+			return nil, fmt.Errorf("don`t read file %w", err)
 		}
 		return &Storage{m: m, wr: writer}, nil
 	}
