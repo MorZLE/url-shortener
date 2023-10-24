2f3e977312f0a17e9989eab2ffb78d0f2d06f879
Артем <art04.04@mail.ru>
2023-10-24 10:17:24 +0300

    fix

--- internal/consts/constErr.go
+++ internal/consts/constErr.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrNotFound      = errors.New("not found")
-	ErrAlreadyExists = errors.New("key Busy")
+	ErrAlreadyExists = errors.New("key already exists")
 	ErrGenShort      = errors.New("failed to generate short URL")
 	ErrGetURL        = errors.New("failed to get URL")
 	ErrDuplicateURL  = errors.New("duplicate URL")
